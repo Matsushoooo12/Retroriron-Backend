830dde68586cbf0d7275d327a73723d83a11ec95
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-31 01:58:42 +0900

    Imageモデル削除

--- app/controllers/api/v1/images_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::ImagesController < ApplicationController
-    def index
-        images = Image.all.order(created_at: :desc)
-        render json: images
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :images, only: %i[index]
       resources :news, only: %i[index]
       resources :lives, only: %i[index]
       resources :discographies, only: %i[index]
