9e5481c0fabe277d77d900d6a977a142d84c7eb2
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-30 23:07:35 +0900

    Basic認証

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,7 +7,7 @@ RailsAdmin.config do |config|
   ## == Devise ==
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == ENV['USER_NAME'] && password == ENV['USER_PASS']
+      username == "username" && password == "password"
     end
   end
   # config.authenticate_with do
