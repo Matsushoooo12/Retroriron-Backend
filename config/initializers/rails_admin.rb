4836e6b6fb0af5a3a0d097f4ccd6f14444ae9398
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-11-08 17:00:15 +0900

    Basic認証　ユーザー追加

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,13 +7,7 @@ RailsAdmin.config do |config|
   ## == Devise ==
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      (
-        ActiveSupport::SecurityUtils.secure_compare(username, ENV["USER_NAME1"]) &
-        ActiveSupport::SecurityUtils.secure_compare(password, ENV["USER_PASSWORD1"])
-      ) | (
-        ActiveSupport::SecurityUtils.secure_compare(username, ENV["USER_NAME2"]) &
-        ActiveSupport::SecurityUtils.secure_compare(password, ENV["USER_PASSWORD2"])
-      )
+      username == ENV["USER_NAME2"] && ENV["USER_PASSWORD2"]
     end
   end
   # config.authenticate_with do
