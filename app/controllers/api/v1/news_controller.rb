a709df0de6bd8fc1ee182782700c043861172e78
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-08-20 14:31:52 +0900

    日付順

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::NewsController < ApplicationController
     def index
-        news = News.all.order(created_at: :desc)
+        news = News.all.order(date: :desc)
         render json: news
     end
 end
